b79baddc560dd08396758c6aecfce2b13792074e
Aleksandr <aldanchenko@gmail.com>
2020-06-10 16:44:30 +0300

    Feat: added initial version of SettingsDialog with Snyk CLI setup and download.

--- src/main/scala/io/snyk/plugin/ui/settings/SettingsDialog.scala
+++ src/main/scala/io/snyk/plugin/ui/settings/SettingsDialog.scala
@@ -3,18 +3,21 @@ package io.snyk.plugin.ui.settings
 import java.net.URL
 
 import com.intellij.openapi.ui.{ComponentValidator, ValidationInfo}
-import javax.swing.{JCheckBox, JComponent, JLabel, JPanel, JTextField}
+import javax.swing.{JButton, JCheckBox, JComponent, JLabel, JPanel, JSplitPane, JTextField}
 import java.awt.{Dimension, Insets}
 
 import com.intellij.openapi.project.Project
-import com.intellij.ui.DocumentAdapter
+
+import com.intellij.ui.{DocumentAdapter, IdeBorderFactory}
 import com.intellij.uiDesigner.core.{GridConstraints => UIGridConstraints, GridLayoutManager => UIGridLayoutManager}
 import com.intellij.uiDesigner.core.Spacer
+
 import javax.swing.event.DocumentEvent
 
 class SettingsDialog(project: Project) {
 
-  private val persistentState: SnykPersistentStateComponent = SnykPersistentStateComponent.getInstance(project)
+  private val persistentState: SnykPersistentStateComponent =
+    SnykPersistentStateComponent.getInstance(project)
 
   private val customEndpointTextField: JTextField = new JTextField()
   private val organizationTextField: JTextField = new JTextField()
@@ -50,7 +53,7 @@ class SettingsDialog(project: Project) {
   }
 
   def isModified: Boolean =
-     isCustomEndpointModified || isOrganizationModified || isIgnoreUnknownCAModified
+    isCustomEndpointModified || isOrganizationModified || isIgnoreUnknownCAModified
 
   private def isCustomEndpointModified =
     customEndpointTextField.getText != persistentState.getCustomEndpointUrl()
@@ -62,137 +65,332 @@ class SettingsDialog(project: Project) {
     ignoreUnknownCACheckBox.isSelected != persistentState.isIgnoreUnknownCA()
 
   private def setupUI(): Unit = {
-    val gridLayoutManager = new UIGridLayoutManager(5, 2, new Insets(0, 0, 0, 0), -1, -1)
+    val defaultTextFieldWidth = 500
+    rootPanel.setLayout(
+      new UIGridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1))
 
-    rootPanel.setLayout(gridLayoutManager)
+    val mainSettingsSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT)
+
+    mainSettingsSplitPane.setOneTouchExpandable(true)
+    mainSettingsSplitPane.setDividerLocation(200)
+    mainSettingsSplitPane.setDividerSize(3)
+
+    rootPanel.add(
+      mainSettingsSplitPane,
+      new UIGridConstraints(
+        0,
+        0,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW,
+        null,
+        new Dimension(200, 200),
+        null,
+        0,
+        false
+      )
+    )
+
+    val cliConfigurationPanel = new JPanel
+    cliConfigurationPanel.setLayout(
+      new UIGridLayoutManager(4, 3, new Insets(0, 0, 0, 0), -1, -1))
+    mainSettingsSplitPane.setLeftComponent(cliConfigurationPanel)
+    cliConfigurationPanel.setBorder(IdeBorderFactory.createTitledBorder("CLI Configuration"))
 
     val customEndpointLabel = new JLabel
-    customEndpointLabel.setText("Custom endpoint:")
+    customEndpointLabel.setText("Custom Endpoint:")
+    cliConfigurationPanel.add(
+      customEndpointLabel,
+      new UIGridConstraints(
+        0,
+        0,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_NONE,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(120, 16),
+        null,
+        0,
+        false
+      )
+    )
 
-    rootPanel.add(customEndpointLabel, new UIGridConstraints(
-      0,
-      0,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_NONE,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      new Dimension(110, 16),
-      null,
-      0,
-      false))
+    val cliConfigurationSpacer = new Spacer
+    cliConfigurationPanel.add(
+      cliConfigurationSpacer,
+      new UIGridConstraints(
+        3,
+        0,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_VERTICAL,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW,
+        null,
+        null,
+        null,
+        0,
+        false
+      )
+    )
 
-    val spacer = new Spacer()
+    customEndpointTextField.setEditable(true)
+    cliConfigurationPanel.add(
+      customEndpointTextField,
+      new UIGridConstraints(
+        0,
+        1,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(defaultTextFieldWidth, -1),
+        null,
+        0,
+        false
+      )
+    )
 
-    rootPanel.add(spacer, new UIGridConstraints(
-      4,
-      0,
-      1,
-      2,
-      UIGridConstraints.ANCHOR_CENTER,
-      UIGridConstraints.FILL_VERTICAL,
-      1,
-      UIGridConstraints.SIZEPOLICY_WANT_GROW,
-      null,
-      new Dimension(110, 14),
-      null,
-      0,
-      false))
-
-    rootPanel.add(customEndpointTextField, new UIGridConstraints(
-      0,
-      1,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_HORIZONTAL,
-      UIGridConstraints.SIZEPOLICY_WANT_GROW,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      new Dimension(150, -1),
-      null,
-      0,
-      false))
+    ignoreUnknownCACheckBox.setText("Ignore unknown CA")
+    cliConfigurationPanel.add(
+      ignoreUnknownCACheckBox,
+      new UIGridConstraints(
+        1,
+        1,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_NONE,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        null,
+        null,
+        0,
+        false
+      )
+    )
 
     val organizationLabel = new JLabel
     organizationLabel.setText("Organization:")
+    cliConfigurationPanel.add(
+      organizationLabel,
+      new UIGridConstraints(
+        2,
+        0,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_NONE,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        null,
+        null,
+        0,
+        false
+      )
+    )
 
-    rootPanel.add(organizationLabel, new UIGridConstraints(
-      2,
-      0,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_NONE,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      null,
-      null,
-      0,
-      false))
-    rootPanel.add(organizationTextField, new UIGridConstraints(
-      2,
-      1,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_HORIZONTAL,
-      UIGridConstraints.SIZEPOLICY_WANT_GROW,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      new Dimension(150, -1),
-      null,
-      0,
-      false))
+    cliConfigurationPanel.add(
+      organizationTextField,
+      new UIGridConstraints(
+        2,
+        1,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(defaultTextFieldWidth, -1),
+        null,
+        0,
+        false
+      )
+    )
 
-    ignoreUnknownCACheckBox.setText("Ignore unknown CA")
+    val cliSetupPanel = new JPanel
+    cliSetupPanel.setLayout(
+      new UIGridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1))
+    mainSettingsSplitPane.setRightComponent(cliSetupPanel)
+    cliSetupPanel.setBorder(IdeBorderFactory.createTitledBorder("CLI Setup"))
 
-    rootPanel.add(ignoreUnknownCACheckBox, new UIGridConstraints(
-      1,
-      1,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_NONE,
-      UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      null,
-      null,
-      0,
-      false))
+    val versionLabel = new JLabel
+    versionLabel.setText("Version:")
+    cliSetupPanel.add(
+      versionLabel,
+      new UIGridConstraints(
+        0,
+        0,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_NONE,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(120, 16),
+        null,
+        0,
+        false
+      )
+    )
+
+    val cliSetupSpacer = new Spacer
+    cliSetupPanel.add(
+      cliSetupSpacer,
+      new UIGridConstraints(
+        2,
+        0,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_VERTICAL,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW,
+        null,
+        null,
+        null,
+        0,
+        false
+      )
+    )
+
+    val versionTextField = new JTextField
+    versionTextField.setEditable(false)
+    versionTextField.setEnabled(true)
+    cliSetupPanel.add(
+      versionTextField,
+      new UIGridConstraints(
+        0,
+        1,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(defaultTextFieldWidth, -1),
+        null,
+        0,
+        false
+      )
+    )
+
+    val cliPathLabel = new JLabel
+    cliPathLabel.setText("Path:")
+    cliSetupPanel.add(
+      cliPathLabel,
+      new UIGridConstraints(
+        1,
+        0,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_NONE,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        null,
+        null,
+        0,
+        false
+      )
+    )
+
+    val cliPathTextField = new JTextField
+    cliSetupPanel.add(
+      cliPathTextField,
+      new UIGridConstraints(
+        1,
+        1,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(defaultTextFieldWidth, -1),
+        null,
+        0,
+        false
+      )
+    )
+
+    val downloadButton = new JButton
+    downloadButton.setText("Download")
+    cliSetupPanel.add(
+      downloadButton,
+      new UIGridConstraints(
+        0,
+        2,
+        1,
+        1,
+        com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER,
+        com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW,
+        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(115, -1),
+        null,
+        0,
+        false
+      )
+    )
+
+    customEndpointLabel.setLabelFor(customEndpointTextField)
+    organizationLabel.setLabelFor(organizationTextField)
+
+    cliPathLabel.setLabelFor(cliPathTextField)
   }
 
   private def setupValidation(): Unit = {
-    new ComponentValidator(project).withValidator(() => {
+    new ComponentValidator(project)
+      .withValidator(() => {
 
-      if (isUrlValid(customEndpointTextField.getText)) {
-        null
+        if (isUrlValid(customEndpointTextField.getText)) {
+          null
+        } else {
+          new ValidationInfo("Invalid custom enpoint URL.",
+                             customEndpointTextField)
+        }
+      })
+      .installOn(customEndpointTextField)
+
+    customEndpointTextField.getDocument.addDocumentListener(
+      new DocumentAdapter() {
+        protected def textChanged(documentEvent: DocumentEvent): Unit =
+          ComponentValidator
+            .getInstance(customEndpointTextField)
+            .ifPresent((componentValidator: ComponentValidator) =>
+              componentValidator.revalidate())
+      })
+  }
+
+  private def isUrlValid(url: String): Boolean =
+    try {
+      if (url.nonEmpty) {
+        new URL(url).toURI
+
+        true
       } else {
-        new ValidationInfo("Invalid custom enpoint URL.", customEndpointTextField)
+        true
       }
-    }).installOn(customEndpointTextField)
-
-    customEndpointTextField.getDocument.addDocumentListener(new DocumentAdapter() {
-      protected def textChanged(documentEvent: DocumentEvent): Unit =
-        ComponentValidator
-          .getInstance(customEndpointTextField)
-          .ifPresent((componentValidator: ComponentValidator) => componentValidator.revalidate())
-    })
-  }
-
-  private def isUrlValid(url: String): Boolean = try {
-    if (url.nonEmpty) {
-      new URL(url).toURI
-
-      true
-    } else {
-      true
+    } catch {
+      case _: Throwable => false
     }
-  } catch {
-    case _: Throwable => false
-  }
 }
